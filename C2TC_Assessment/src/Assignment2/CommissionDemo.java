ca925db30cbc1f9d380742f6e66c58a6d47acf2d
harshithakrb <harshitha.krb@gmail.com>
2025-07-23 21:11:55 +0530

    This is the assignment 2 of C2TC CoreJavaProgram

---
+++ C2TC_Assessment/src/Assignment2/CommissionDemo.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+public class CommissionDemo {
+
+	public static void main(String[] args) {
+		Commission c  = new Commission();
+		c.details();
+		c.calculate();
+		
+		System.out.println("Successfully completed!!");
+
+	}
+
+}
